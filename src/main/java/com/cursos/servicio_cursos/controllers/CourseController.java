bb4081ac05ec9d5a1831dfbc5474018004b736f8
brayan <brayanjaraba2006@gmail.com>
2026-04-27 11:41:40 -0500

    chore: refactor code

--- src/main/java/com/cursos/servicio_cursos/controllers/CourseController.java
+++ src/main/java/com/cursos/servicio_cursos/controllers/CourseController.java
@@ -1,6 +1,10 @@
 package com.cursos.servicio_cursos.controllers;
 
 import com.cursos.servicio_cursos.services.CourseService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import com.cursos.servicio_cursos.entities.CourseEntity;
 import com.cursos.servicio_cursos.dtos.RequestCourse;
 import com.cursos.servicio_cursos.dtos.ResponseCourse;
@@ -10,19 +14,29 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 
-@RestController // Anotación para indicar que esta clase es un controlador REST para manejar solicitudes HTTP
-@RequestMapping("/api/courses") // Anotación para definir la ruta base para todas las solicitudes relacionadas con cursos
-
-
+@RestController // Anotación para indicar que esta clase es un controlador REST para manejar
+                // solicitudes HTTP
+@RequestMapping("/api/courses") // Anotación para definir la ruta base para todas las solicitudes relacionadas
+                                // con cursos
+@RequiredArgsConstructor
+@Slf4j
 public class CourseController {
-    
-    @Autowired
-    private CourseService courseService;
+
+    private final CourseService courseService;
 
     @PostMapping // Anotación para manejar solicitudes HTTP POST para crear un nuevo curso
-    public ResponseEntity<CourseEntity> createCourse (@RequestBody RequestCourse requestCourse) { // toma la peticion http y lo convierte en un objeto de tipo RequestCourse, que es un DTO que contiene los datos necesarios para crear un curso
+    public ResponseEntity<CourseEntity> createCourse(@RequestBody RequestCourse requestCourse) { // toma la peticion
+                                                                                                 // http y lo convierte
+                                                                                                 // en un objeto de tipo
+                                                                                                 // RequestCourse, que
+                                                                                                 // es un DTO que
+                                                                                                 // contiene los datos
+                                                                                                 // necesarios para
+                                                                                                 // crear un curso
+        log.info("CONTROLLER: request: {}", requestCourse);
         CourseEntity newCourse = courseService.createCourse(requestCourse);
-        return new ResponseEntity<>(newCourse, HttpStatus.CREATED); // Devuelve el curso creado con el estado HTTP 201 (CREATED) aun no entiendo bien lo de los estados. 
+        return new ResponseEntity<>(newCourse, HttpStatus.CREATED); // Devuelve el curso creado con el estado HTTP 201
+                                                                    // (CREATED) aun no entiendo bien lo de los estados.
     }
 
     @GetMapping // Anotación para manejar solicitudes HTTP GET para obtener la lista de cursos
