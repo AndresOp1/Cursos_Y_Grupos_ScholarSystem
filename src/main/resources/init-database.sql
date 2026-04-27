09fe8403b96de0370122a47ed57e4f0aa7aea9da
brayan <brayanjaraba2006@gmail.com>
2026-04-27 07:47:20 -0500

    feat: create databse creator script

---
+++ src/main/resources/init-database.sql
@@ -0,0 +1,39 @@
+CREATE SCHEMA IF NOT EXISTS courses_groups;
+
+CREATE TABLE IF NOT EXISTS courses_groups.roles (
+    role_id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS courses_groups.users (
+    user_id BIGSERIAL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    full_name VARCHAR(200) NOT NULL,
+    role BIGINT NOT NULL REFERENCES courses_groups.roles (role_id)
+);
+
+CREATE TABLE IF NOT EXISTS courses_groups.courses (
+    code BIGSERIAL PRIMARY KEY,
+    name VARCHAR(200) UNIQUE NOT NULL,
+    credits INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS courses_groups.groups (
+    group_id BIGSERIAL PRIMARY KEY,
+    teacher_id BIGINT REFERENCES courses_groups.users (user_id),
+    course_code BIGINT REFERENCES courses_groups.courses (code)
+);
+
+CREATE TABLE courses_groups.inscriptions (
+    group_id BIGINT REFERENCES courses_groups.groups (group_id),
+    student_id BIGINT REFERENCES courses_groups.users (user_id),
+    PRIMARY KEY (group_id, student_id)
+)
+
+CREATE TABLE IF NOT EXISTS courses_groups.schedules (
+    schedule_id BIGSERIAL PRIMARY KEY,
+    day VARCHAR(50) NOT NULL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL,
+    group_id BIGINT NOT NULL UNIQUE REFERENCES courses_groups.groups (group_id)
+);
