b2e2474e6f580d4dfee50f78a997caa136b18e92
brayan <brayanjaraba2006@gmail.com>
2026-04-27 08:14:19 -0500

    feat: add lombook to class fix: attribuites to match with database

--- src/main/java/com/cursos/servicio_cursos/entities/GroupEntity.java
+++ src/main/java/com/cursos/servicio_cursos/entities/GroupEntity.java
@@ -6,24 +6,29 @@
 package com.cursos.servicio_cursos.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.List;
 
 @Entity
 @Table(name = "groups", schema = "courses_groups")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class GroupEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false, unique = true)
-    private String code; // Codigo unico del grupo
+    @Column(nullable = false, unique = true, name = "group_id")
+    private long groupId; // Codigo unico del grupo
 
     @Column(nullable = false)
     private String name; // Nombre del grupo
-
-    @Column(nullable = false)
-    private int credits; // Creditos del grupo
 
     // la recion es que muchos grupos pertenecen a un curos entonces seria ManyToOne
     @ManyToOne
@@ -44,82 +49,5 @@ public class GroupEntity {
     // OneToMany
     @OneToMany(mappedBy = "group")
     private List<InscriptionEntity> inscriptions; // Las inscripciones al grupo
-
-    // Constructor vacío
-    public GroupEntity() {
-    }
-
-    // Constructor con campos básicos que serian el code, name, curso
-    public GroupEntity(String code, String name, CourseEntity course) {
-        this.code = code;
-        this.name = name;
-        this.course = course;
-
-    }
-
-    // Getters y Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getCredits() {
-        return credits;
-    }
-
-    public void setCredits(int credits) {
-        this.credits = credits;
-    }
-
-    public CourseEntity getCourse() {
-        return course;
-    }
-
-    public void setCourse(CourseEntity course) {
-        this.course = course;
-    }
-
-    public UserEntity getTeacher() {
-        return teacher;
-    }
-
-    public void setTeacher(UserEntity teacher) {
-        this.teacher = teacher;
-    }
-
-    public List<ScheduleEntity> getSchedules() {
-        return schedules;
-    }
-
-    public void setSchedules(List<ScheduleEntity> schedules) {
-        this.schedules = schedules;
-    }
-
-    public List<InscriptionEntity> getInscriptions() {
-        return inscriptions;
-    }
-
-    public void setInscriptions(List<InscriptionEntity> inscriptions) {
-        this.inscriptions = inscriptions;
-    }
 
 }
