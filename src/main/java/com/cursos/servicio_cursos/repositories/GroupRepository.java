9e94c1ad23109881e8b315d47928e817ba88c106
brayan <brayanjaraba2006@gmail.com>
2026-04-27 12:33:26 -0500

    fix: remove unnecesary function

--- src/main/java/com/cursos/servicio_cursos/repositories/GroupRepository.java
+++ src/main/java/com/cursos/servicio_cursos/repositories/GroupRepository.java
@@ -7,10 +7,8 @@ import java.util.List;
 
 @Repository
 public interface GroupRepository extends JpaRepository<GroupEntity, Long> {
-    
-    GroupEntity findByCode(String code); // este método me permite buscar un grupo por su código.
 
     List<GroupEntity> findByTeacherId(long id); // este método me permite buscar grupos por el ID del profesor.
 }
 
-//no se si este repositorio sea necesario 
+// no se si este repositorio sea necesario
