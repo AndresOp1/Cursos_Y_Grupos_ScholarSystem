bbf90b74fbad41ea06c8aee6c0f989affe77aca5
brayan <brayanjaraba2006@gmail.com>
2026-04-27 15:52:31 -0500

    feat: add to Dto function on schedule mapper

--- src/main/java/com/cursos/servicio_cursos/mappers/ScheduleMapper.java
+++ src/main/java/com/cursos/servicio_cursos/mappers/ScheduleMapper.java
@@ -2,17 +2,25 @@ package com.cursos.servicio_cursos.mappers;
 
 import org.springframework.stereotype.Component;
 
-import com.cursos.servicio_cursos.dtos.ScheduleRequest;
+import com.cursos.servicio_cursos.dtos.ScheduleDto;
 import com.cursos.servicio_cursos.entities.ScheduleEntity;
 import com.cursos.servicio_cursos.enums.DayOfWeek;
 
 @Component
 public class ScheduleMapper {
 
-  public ScheduleEntity toEntity(ScheduleRequest req) throws IllegalArgumentException {
+  public ScheduleEntity toEntity(ScheduleDto req) throws IllegalArgumentException {
     return ScheduleEntity.builder()
         .day(DayOfWeek.valueOf(req.day()))
         .timeStar(req.startsTime())
         .timeEnd(req.endTime()).build();
+  }
+
+  public ScheduleDto toDto(ScheduleEntity entity) {
+    return ScheduleDto.builder()
+        .day(entity.getDay().toString())
+        .startsTime(entity.getTimeStar())
+        .endTime(entity.getTimeEnd())
+        .build();
   }
 }
