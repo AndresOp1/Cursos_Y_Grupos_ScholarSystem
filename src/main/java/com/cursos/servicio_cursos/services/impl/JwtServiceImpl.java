75349d1b7c659ebada6719b04046c7468bf1932b
brayan <brayanjaraba2006@gmail.com>
2026-05-02 08:02:51 -0500

    fix: add service anotation

--- src/main/java/com/cursos/servicio_cursos/services/impl/JwtServiceImpl.java
+++ src/main/java/com/cursos/servicio_cursos/services/impl/JwtServiceImpl.java
@@ -3,6 +3,7 @@ package com.cursos.servicio_cursos.services.impl;
 import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import com.cursos.servicio_cursos.services.JwtService;
 
@@ -11,6 +12,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
+@Service
 public class JwtServiceImpl implements JwtService {
 
   @Value("${application.security.jwt.secret-key}")
