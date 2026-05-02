eccd7418124f43cfcdca7654bed924b33cc19754
brayan <brayanjaraba2006@gmail.com>
2026-05-02 15:10:48 -0500

    fix: upsert user

--- src/main/java/com/cursos/servicio_cursos/services/impl/UserServiceImpl.java
+++ src/main/java/com/cursos/servicio_cursos/services/impl/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package com.cursos.servicio_cursos.services.impl;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import com.cursos.servicio_cursos.dtos.UserMessage;
@@ -10,25 +12,37 @@ import com.cursos.servicio_cursos.repositories.RoleRepository;
 import com.cursos.servicio_cursos.repositories.UserRepository;
 import com.cursos.servicio_cursos.services.UserService;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserServiceImpl implements UserService {
   private final UserRepository userRepo;
   private final RoleRepository roleRepo;
 
   @Override
+  @Transactional
   public void upsertUser(UserMessage userMessage) {
+    log.info("procesando usuario de id: {}", userMessage.id());
     RoleEntity role = roleRepo.findByName(userMessage.role())
         .orElseThrow(() -> new InvalidRoleException(userMessage.role()));
 
-    UserEntity userEntity = userRepo.findById(userMessage.id())
-        .orElse(UserEntity.builder().id(userMessage.id()).build());
+    Optional<UserEntity> userEntity = userRepo.findById(userMessage.id());
+    if (userEntity.isPresent()) {
+      UserEntity user = userEntity.get();
+      userRepo.updateUser(user.getId(), userMessage.fullName(), userMessage.email(), role);
+      return;
+    }
+    UserEntity user = UserEntity.builder()
+        .id(userMessage.id())
+        .email(userMessage.email())
+        .fullName(userMessage.fullName())
+        .role(role)
+        .build();
+    userRepo.save(user);
 
-    userEntity.setEmail(userMessage.email());
-    userEntity.setFullName(userMessage.fullName());
-    userEntity.setRole(role);
-    userRepo.save(userEntity);
   }
 }
