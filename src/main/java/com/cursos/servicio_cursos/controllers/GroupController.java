7ab9726d78b859f163b98199b904bb8b1c30311a
brayan <brayanjaraba2006@gmail.com>
2026-05-02 15:11:23 -0500

    chore: remove commentaries

--- src/main/java/com/cursos/servicio_cursos/controllers/GroupController.java
+++ src/main/java/com/cursos/servicio_cursos/controllers/GroupController.java
@@ -6,7 +6,6 @@ import com.cursos.servicio_cursos.services.GroupService;
 
 import lombok.RequiredArgsConstructor;
 
-import com.cursos.servicio_cursos.entities.GroupEntity;
 import com.cursos.servicio_cursos.dtos.RequestGroup;
 import com.cursos.servicio_cursos.dtos.ResponseGroup;
 
@@ -20,25 +19,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController // Anotación para indicar que esta clase es un controlador REST para manejar
-                // solicitudes HTTP
-@RequestMapping("/api/groups") // Anotación para definir la ruta base para todas las solicitudes relacionadas
-                               // con grupos
+@RestController
+@RequestMapping("/api/groups")
 @RequiredArgsConstructor
 public class GroupController {
 
     private final GroupService groupService;
 
-    // POST para crear un nuevo grupo
     @PostMapping
-    public ResponseEntity<ResponseGroup> createGroup(@RequestBody RequestGroup requestGroup) { // toma la peticion http
-                                                                                               // y lo convierte en un
-                                                                                               // objeto de tipo
-                                                                                               // RequestGroup, que es
-        // un DTO que contiene los datos necesarios para crear un grupo
+    public ResponseEntity<ResponseGroup> createGroup(@RequestBody RequestGroup requestGroup) {
         ResponseGroup newGroup = groupService.createGroup(requestGroup);
-        return new ResponseEntity<>(newGroup, HttpStatus.CREATED); // Devuelve el grupo creado con el estado HTTP 201
-                                                                   // (CREATED)
+        return new ResponseEntity<>(newGroup, HttpStatus.CREATED);
     }
 
     @GetMapping("/{courseCode}")
@@ -46,21 +37,8 @@ public class GroupController {
         return ResponseEntity.ok(groupService.findGroupsByCourseCode(courseCode));
     }
 
-    @PutMapping("{groupId}/teacher/{teacherId}") // Anotación para manejar solicitudes HTTP PUT para asignar un profesor
-                                                 // a un grupo, se recibe el ID del
-    // grupo y el ID del profesor en la URL|
-    public ResponseEntity<GroupEntity> assignTeacher(@PathVariable Long groupId, @PathVariable Long teacherId) { // toma
-                                                                                                                 // los
-                                                                                                                 // ID
-                                                                                                                 // del
-                                                                                                                 // grupo
-                                                                                                                 // y
-                                                                                                                 // del
-                                                                                                                 // profesor
-                                                                                                                 // de
-                                                                                                                 // la
-                                                                                                                 // URL
-        GroupEntity updatedGroup = groupService.assignTeacher(groupId, teacherId);
-        return ResponseEntity.ok(updatedGroup); // Devuelve el grupo actualizado con el estado HTTP 200 (OK)
+    @PutMapping("{groupId}/teacher/{teacherId}")
+    public ResponseEntity<ResponseGroup> assignTeacher(@PathVariable Long groupId, @PathVariable Long teacherId) {
+        return ResponseEntity.ok(groupService.assignTeacher(teacherId, groupId));
     }
 }
