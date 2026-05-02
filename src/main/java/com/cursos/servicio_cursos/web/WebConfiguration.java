6b2b367612d4f5af32b782098d9fd18db15677bc
brayan <brayanjaraba2006@gmail.com>
2026-05-02 08:02:30 -0500

    feat: create queue client for azure queue

--- src/main/java/com/cursos/servicio_cursos/web/WebConfiguration.java
+++ src/main/java/com/cursos/servicio_cursos/web/WebConfiguration.java
@@ -5,15 +5,31 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.azure.storage.queue.QueueClient;
+import com.azure.storage.queue.QueueClientBuilder;
+
 @Configuration
 public class WebConfiguration {
   @Value("${authentication.service.url}")
   private String authenticationServiceUrl;
+
+  @Value("${spring.cloud.azure.storage.connection-string}")
+  private String connectionString;
+  @Value("${spring.cloud.azure.storage.queue.queue-name}")
+  private String queueName;
 
   @Bean
   public WebClient authenticationWebClient() {
     return WebClient.builder()
         .baseUrl(authenticationServiceUrl)
         .build();
+  }
+
+  @Bean
+  public QueueClient queueClient() {
+    return new QueueClientBuilder()
+        .connectionString(connectionString)
+        .queueName(queueName)
+        .buildClient();
   }
 }
