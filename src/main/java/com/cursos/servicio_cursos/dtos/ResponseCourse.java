8f0a541a0f7bf0ef9a2ec9f5192cd3c64a1d2e9a
brayan <brayanjaraba2006@gmail.com>
2026-05-13 10:01:12 -0500

    refactor: convert response course to class

--- src/main/java/com/cursos/servicio_cursos/dtos/ResponseCourse.java
+++ src/main/java/com/cursos/servicio_cursos/dtos/ResponseCourse.java
@@ -1,10 +1,24 @@
 package com.cursos.servicio_cursos.dtos;
 
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Builder
-public record ResponseCourse(
-                Long code,
-                String name,
-                int credits) {
+@Getter
+@Setter
+@NoArgsConstructor
+public class ResponseCourse {
+    private Long code;
+    private String name;
+    private int credits;
+    private Long ngroups;
+
+    public ResponseCourse(Long code, String name, int credits, Long ngroups) {
+        this.code = code;
+        this.name = name;
+        this.credits = credits;
+        this.ngroups = ngroups;
+    }
 }
