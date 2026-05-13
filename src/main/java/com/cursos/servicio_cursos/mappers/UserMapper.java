d1ef9a57d99666752f39f847b82cd702fc62de9c
brayan <brayanjaraba2006@gmail.com>
2026-05-13 09:59:38 -0500

    feat: user mapper from entity to dto

---
+++ src/main/java/com/cursos/servicio_cursos/mappers/UserMapper.java
@@ -0,0 +1,19 @@
+package com.cursos.servicio_cursos.mappers;
+
+import org.springframework.stereotype.Component;
+
+import com.cursos.servicio_cursos.dtos.UserResponse;
+import com.cursos.servicio_cursos.entities.UserEntity;
+
+@Component
+public class UserMapper {
+
+  public UserResponse fomEntityToResponse(UserEntity entity) {
+    return UserResponse.builder()
+        .fullName(entity.getFullName())
+        .email(entity.getEmail())
+        .id(entity.getId())
+        .role(entity.getRole().getName())
+        .build();
+  }
+}
