163d879ffc3c593075fb182e269f710dd9c5556b
brayan <brayanjaraba2006@gmail.com>
2026-05-14 16:35:35 -0500

    chore: format code feature: add update group logic

--- src/main/java/com/cursos/servicio_cursos/controllers/GroupController.java
+++ src/main/java/com/cursos/servicio_cursos/controllers/GroupController.java
@@ -2,25 +2,18 @@
 
 package com.cursos.servicio_cursos.controllers;
 
-import com.cursos.servicio_cursos.services.GroupService;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
 import com.cursos.servicio_cursos.dtos.GroupDetails;
 import com.cursos.servicio_cursos.dtos.RequestGroup;
-import com.cursos.servicio_cursos.dtos.RequestStudentsInscriptions;
 import com.cursos.servicio_cursos.dtos.ResponseGroup;
-
-import java.util.List;
-
+import com.cursos.servicio_cursos.dtos.UpdateGroupRequest;
+import com.cursos.servicio_cursos.services.GroupService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/groups")
@@ -28,34 +21,36 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Slf4j
 public class GroupController {
 
-    private final GroupService groupService;
+  private final GroupService groupService;
 
-    @PostMapping
-    public ResponseEntity<ResponseGroup> createGroup(@RequestBody RequestGroup requestGroup) {
-        ResponseGroup newGroup = groupService.createGroup(requestGroup);
-        return new ResponseEntity<>(newGroup, HttpStatus.CREATED);
-    }
+  @PostMapping
+  public ResponseEntity<ResponseGroup> createGroup(@RequestBody RequestGroup requestGroup) {
+    ResponseGroup newGroup = groupService.createGroup(requestGroup);
+    return new ResponseEntity<>(newGroup, HttpStatus.CREATED);
+  }
 
-    @GetMapping("course/{courseCode}")
-    public ResponseEntity<List<ResponseGroup>> findGoupsByCourseId(@PathVariable Long courseCode) {
-        return ResponseEntity.ok(groupService.findGroupsByCourseCode(courseCode));
-    }
+  @GetMapping("course/{courseCode}")
+  public ResponseEntity<List<ResponseGroup>> findGoupsByCourseId(@PathVariable Long courseCode) {
+    return ResponseEntity.ok(groupService.findGroupsByCourseCode(courseCode));
+  }
 
-    @GetMapping("/{groupId}")
-    public ResponseEntity<GroupDetails> getGroupDetails(@PathVariable Long groupId) {
-        log.info("Peticion de detalles del grupo con id: {}", groupId);
-        GroupDetails groupDetails = groupService.getGroupDetails(groupId);
-        return ResponseEntity.ok(groupDetails);
-    }
+  @GetMapping("/{groupId}")
+  public ResponseEntity<GroupDetails> getGroupDetails(@PathVariable Long groupId) {
+    log.info("Peticion de detalles del grupo con id: {}", groupId);
+    GroupDetails groupDetails = groupService.getGroupDetails(groupId);
+    return ResponseEntity.ok(groupDetails);
+  }
 
-    @PutMapping("{groupId}/teacher/{teacherId}")
-    public ResponseEntity<ResponseGroup> assignTeacher(@PathVariable Long groupId, @PathVariable Long teacherId) {
-        return ResponseEntity.ok(groupService.assignTeacher(teacherId, groupId));
-    }
+  @PutMapping("/{groupId}")
+  public ResponseEntity<Void> updateGroup(@PathVariable Long groupId,
+                                          @RequestBody UpdateGroupRequest requestUpdate) {
+    log.info("update group request, group id {}, request {}", groupId, requestUpdate);
+    groupService.updateGroup(groupId, requestUpdate);
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
 
-    @PostMapping("/inscriptions")
-    public ResponseEntity<Void> inscribeStudents(@RequestBody RequestStudentsInscriptions requestBody) {
-        groupService.assignStudents(requestBody);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
-    }
+  @PutMapping("{groupId}/teacher/{teacherId}")
+  public ResponseEntity<ResponseGroup> assignTeacher(@PathVariable Long groupId, @PathVariable Long teacherId) {
+    return ResponseEntity.ok(groupService.assignTeacher(teacherId, groupId));
+  }
 }
