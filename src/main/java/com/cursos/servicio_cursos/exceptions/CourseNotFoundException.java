66667e3b631e58b30b7fc9f7732ec1f06832483d
brayan <brayanjaraba2006@gmail.com>
2026-05-14 16:34:47 -0500

    feat: new constructor tor course not found exception with course code

--- src/main/java/com/cursos/servicio_cursos/exceptions/CourseNotFoundException.java
+++ src/main/java/com/cursos/servicio_cursos/exceptions/CourseNotFoundException.java
@@ -4,4 +4,8 @@ public class CourseNotFoundException extends RuntimeException {
   public CourseNotFoundException() {
     super("Curso no encontrado");
   }
+
+  public CourseNotFoundException(Long code) {
+    super(String.format("Curso de codigo %d no encontrado", code));
+  }
 }
