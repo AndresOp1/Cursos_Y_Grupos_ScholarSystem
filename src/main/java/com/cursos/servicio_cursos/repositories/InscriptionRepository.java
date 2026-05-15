79da78198e8a6fd80071c1a8cbbec2f7796c4e12
brayan <brayanjaraba2006@gmail.com>
2026-05-15 14:28:45 -0500

    refactor: delete not used function

--- src/main/java/com/cursos/servicio_cursos/repositories/InscriptionRepository.java
+++ src/main/java/com/cursos/servicio_cursos/repositories/InscriptionRepository.java
@@ -4,9 +4,7 @@ import com.cursos.servicio_cursos.entities.CourseEntity;
 import com.cursos.servicio_cursos.entities.InscriptionEntity;
 import com.cursos.servicio_cursos.entities.InscriptionId;
 
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -19,14 +17,6 @@ public interface InscriptionRepository extends JpaRepository<InscriptionEntity, 
 
   @Query("SELECT DISTINCT i.group.course FROM InscriptionEntity i WHERE i.user.id = :userId")
   List<CourseEntity> findCoursesByUserId(@Param("userId") Long userId);
-
-  @Modifying
-  @Transactional
-  @Query("""
-          DELETE FROM InscriptionEntity i
-          WHERE i.group.groupId = :groupId
-          """)
-  void deleteByGroupId(@Param("groupId") Long groupId);
 
   List<InscriptionEntity> findAllByGroupId(Long groupId);
 }
--- src/main/java/com/cursos/servicio_cursos/repositories/ScheduleRepository.java
+++ src/main/java/com/cursos/servicio_cursos/repositories/ScheduleRepository.java
@@ -2,25 +2,13 @@ package com.cursos.servicio_cursos.repositories;
 
 import com.cursos.servicio_cursos.entities.GroupEntity;
 import com.cursos.servicio_cursos.entities.ScheduleEntity;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ScheduleRepository extends JpaRepository<ScheduleEntity, Long> {
-
-  @Modifying
-  @Transactional
-  @Query("""
-          DELETE FROM ScheduleEntity s
-          WHERE s.group.groupId = :groupId
-          """)
-  void deleteAllByGroupId(@Param("groupId") Long groupId);
 
 
   List<ScheduleEntity> findAllByGroup(GroupEntity group);
